981278656cd2680666bc87bd8cbaf15209af236b
02Adri <palmabenavidesa650@gmail.com>
2025-03-22 21:17:45 -0600

    creando vista previa para el registro de entrada y salida

--- src/app/modal-excel/modal-excel.component.ts
+++ src/app/modal-excel/modal-excel.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import {ModalController,IonicModule} from '@ionic/angular'
+import{IonHeader,IonToolbar,IonTitle,IonButtons,IonButton,IonIcon,IonContent,IonCard,IonCardContent, IonCardHeader, IonCardTitle }from '@ionic/angular/standalone'
+import { closeOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-modal-excel',
   templateUrl: './modal-excel.component.html',
   styleUrls: ['./modal-excel.component.scss'],
   standalone: true,
+  imports:[IonHeader,IonToolbar,IonTitle,IonButtons,IonButton,IonIcon,IonContent,IonCard,IonCardContent,IonCardHeader,IonCardTitle,CommonModule,IonicModule]
 })
 export class ModalExcelComponent  implements OnInit {
-
-  constructor() { }
+   
+  @Input() datosPrevios:any[]=[]
+  constructor(private modalCtrl:ModalController) { 
+    addIcons({closeOutline})
+  }
 
   ngOnInit() {}
+  cerrarModal(){  
+    this.modalCtrl.dismiss()
+  }
 
 }
