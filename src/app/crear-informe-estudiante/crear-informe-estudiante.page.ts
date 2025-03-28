72f9ab3d8a590b9454b85173df90d9951b68ea35
02Adri <palmabenavidesa650@gmail.com>
2025-03-27 21:53:08 -0600

    agregando menu slide y estilos al popover

--- src/app/crear-informe-estudiante/crear-informe-estudiante.page.ts
+++ src/app/crear-informe-estudiante/crear-informe-estudiante.page.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonBackButton,IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonBackButton,IonButton,IonApp,IonMenu,IonMenuButton,IonIcon} from '@ionic/angular/standalone';
 import {AppLauncher} from '@capacitor/app-launcher'
 import {Platform} from '@ionic/angular'
 import { addIcons } from 'ionicons';
-import { chevronBackOutline } from 'ionicons/icons';
+import { chevronBackOutline,addCircleOutline } from 'ionicons/icons';
 @Component({
   selector: 'app-crear-informe-estudiante',
   templateUrl: './crear-informe-estudiante.page.html',
   styleUrls: ['./crear-informe-estudiante.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButtons,IonBackButton,IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonButtons,IonBackButton,IonButton,IonApp,IonMenu,IonMenuButton,IonIcon]
 })
 export class CrearInformeEstudiantePage implements OnInit {
 
   constructor(private platform:Platform) { 
-    addIcons({chevronBackOutline})
+    addIcons({chevronBackOutline,addCircleOutline})
   }
 
   ngOnInit() {
